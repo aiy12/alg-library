83ada6c4044c799e80b049cb09cbc66dedbf6aef
Anand Iyer <33114721+aiy12@users.noreply.github.com>
2019-01-03 20:33:51 -0600

    Create Combo.cpp

---
+++ Math/Combo.cpp
@@ -0,0 +1,31 @@
+template<int sz> struct Combo {
+    ll fac[sz + 1];
+    ll ifac[sz + 1];
+
+    Combo() {
+        fac[0] = ifac[0] = 1LL;
+        for (int i = 1; i <= sz; i++) {
+            fac[i] = fac[i - 1] * i % MOD;
+            ifac[i] = mod_inv(fac[i]);
+        }
+    }
+
+    ll mod_pow(ll a, ll b) {
+        if (b == 0) return 1LL;
+        ll p = mod_pow(a, b >> 1);
+        p = p * p % MOD;
+        if (b & 1)
+            p = p * a % MOD;
+        return p;
+    }
+
+    ll mod_inv(ll n) {
+        return mod_pow(n, MOD - 2);
+    }
+
+    ll comb(ll n, ll r) {
+        if (n < r || n < 0 || r < 0) return 0LL;
+        ll res = (fac[n] * ifac[r] % MOD) * ifac[n - r] % MOD;
+        return res;
+    }
+};
