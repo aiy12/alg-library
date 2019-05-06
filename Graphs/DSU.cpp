d617568080d0c91ca9e56c1f93b138eaa8c76d00
Anand Iyer <33114721+aiy12@users.noreply.github.com>
2019-05-05 19:49:42 -0500

    Update DSU.cpp

--- Graphs/DSU.cpp
+++ Graphs/DSU.cpp
@@ -1,21 +1,21 @@
 template<int sz> struct DSU {
-    int par[sz+5];
-    int size[sz+5];
+    int par[sz + 5];
+    int size[sz + 5];
     DSU() {
-        forn(i, sz+5) {
+        forn(i, sz + 5) {
             par[i] = i;
             size[i] = 1;
         }
     }
     int find(int node) { //path compression
-        if(par[node] != node) par[node] = find(par[node]);
+        if (par[node] != node) par[node] = find(par[node]);
         return par[node];
     }
     void unite(int node1, int node2) { //union by rank
         node1 = find(node1);
         node2 = find(node2);
-        if(node1 == node2) return;
-        if(size[node1] < size[node2]) swap(node1, node2);
+        if (node1 == node2) return;
+        if (size[node1] < size[node2]) swap(node1, node2);
         size[node1] += size[node2];
         par[node2] = node1;
     }
