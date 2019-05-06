bf77538149198aae505b63eb3f7a8e1271d18598
Anand Iyer <33114721+aiy12@users.noreply.github.com>
2019-05-05 19:52:12 -0500

    Update BIT.cpp

--- Range Queries/BIT.cpp
+++ Range Queries/BIT.cpp
@@ -1,24 +1,24 @@
 template<int sz> struct BIT {
-	int tree[sz+10];
+	int tree[sz + 10];
 	init() {
 		memset(tree, 0, sizeof(tree));
 	}
 	void upd(int k, int x) {
 		++k;
-		while(k < sz+10) {
+		while (k < sz + 10) {
 			tree[k] += x;
-			k += (k&-k);
+			k += (k & -k);
 		}
 	}
 	int query(int l, int r) {
-		return sum(r) - sum(l-1);
+		return sum(r) - sum(l - 1);
 	}
 	int sum(int q) {
 		++q;
 		int res = 0;
-		while(q >= 1) {
+		while (q >= 1) {
 			res += tree[q];
-			q -= (q&-q);
+			q -= (q & -q);
 		}
 		return res;
 	}
