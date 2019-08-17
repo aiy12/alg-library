093bf56ab153ff32450d50982b56ac86a5643897
Anand Iyer <33114721+aiy12@users.noreply.github.com>
2019-08-17 17:35:35 -0500

    Create KMP.cpp

---
+++ Strings/KMP.cpp
@@ -0,0 +1,38 @@
+vector<int> precomp(string pat) {
+    vector<int> ret(pat.length());
+    ret[0] = 0;
+    int len = 0; // length of prev longest prefix suffix
+    for (int i = 1; i < pat.length(); ) {
+        if (pat[i] == pat[len]) {
+            len++;
+            ret[i] = len;
+            i++;
+        } else if (len != 0) {
+            len = ret[len - 1];
+        } else {
+            ret[i] = 0; i++;
+        }
+    }
+    return ret;
+}
+
+void KMP(string text, string pat) {
+    vector<int> lps = precomp(pat);
+    int i = 0, j = 0;
+    while (i < text.length()) {
+        if (text[i] == pat[j]) {
+            i++; j++;
+        }
+        if (j == pat.length()) {
+            // solution found
+            cout << "sol at index: " << i-j << endl;
+        } else if (i < N && text[i] != pat[j]) {
+            else if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    return;
+}
