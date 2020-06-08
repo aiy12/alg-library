11a995c09c9b39f9b26cbc06ae3e5e1a140f2c5d
Anand Iyer <anandiyer1000@gmail.com>
2020-06-08 15:59:58 -0500

    Update SegmentTree.cpp

--- Range Queries/SegmentTree.cpp
+++ Range Queries/SegmentTree.cpp
@@ -1,19 +1,3 @@
-#include <bits/stdc++.h>
- 
-using namespace std;
- 
-typedef pair<int,int> pii;
-typedef long long ll;
-
-#define forn(i, n) for (int i = 0; i < (n); i++)
-#define ff first
-#define ss second
-#define mp make_pair
-#define pb push_back
- 
-const int MAXN = 5e5 + 10;
-const int MOD = 1e9 + 7;
-
 int N;
 int seg[4 * MAXN];
 
@@ -49,9 +33,7 @@ int query(int node, int left, int right, int start, int end) {
     return query(2*node, left, mid, start, end) + query(2*node+1, mid+1, right, start, end);
 }
 
-int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
- 
+int main() { 
     int arr[N=10] = {1,2,3,4,5,6,7,8,9,10};
     init(arr, 1, 0, N-1); // root of segtree is node 1
     cout << query(1, 0, N-1, 0, 4) << endl;
