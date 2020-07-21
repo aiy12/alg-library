aa08c18db9104aeb186207821f8f4e21cba80a01
Anand Iyer <anandiyer1000@gmail.com>
2020-07-20 19:42:30 -0500

    Update TopoSort.cpp

--- Graphs/TopoSort.cpp
+++ Graphs/TopoSort.cpp
@@ -4,26 +4,26 @@ vector<int> adjList[MAXN];
 int inDeg[MAXN];
 
 vector<int> topoSort() {
-  vector<int> order;
-  queue<int> bfs;
+    vector<int> order;
+    queue<int> bfs;
     
-  // might need to update if 1-indexed
+    // might need to update if 1-indexed
 	for (int node = 0; node < n; node++) {
-	  if (inDeg[node] == 0)
-      bfs.push(node);
+	    if (inDeg[node] == 0)
+	    bfs.push(node);
 	}
-	
+
 	while (!bfs.empty()) {
-	  int curNode = bfs.front();
-	  bfs.pop();
+	    int curNode = bfs.front();
+	    bfs.pop();
 		order.push_back(curNode);
-		
+
 		for (int adjNode : adjList[curNode]) {
 			inDeg[adjNode]--;
 			if (inDeg[adjNode] == 0)
-		    bfs.push(adjNode);
-    }
-  }
-	
-  return order;
+			bfs.push(adjNode);
+		}
+	}
+
+	return order;
 }
