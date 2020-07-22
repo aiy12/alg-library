b5a7bee279f5a86fd5581a70347abf9c56ae6aef
Anand Iyer <anandiyer1000@gmail.com>
2020-07-22 12:27:12 -0500

    Update Kahn.cpp

--- Graphs/Kahn.cpp
+++ Graphs/Kahn.cpp
@@ -2,13 +2,14 @@
 
 vector<int> adjList[MAXN];
 int inDeg[MAXN];
+bool visited[MAXN];
 
 vector<int> topoSort() {
     vector<int> order;
     queue<int> bfs;
     
     // might need to update if 1-indexed
-    for (int node = 0; node < N; node++) {
+    for (int node = 0; node < n; node++) {
         if (inDeg[node] == 0)
             bfs.push(node);
     }
@@ -16,6 +17,9 @@ vector<int> topoSort() {
     while (!bfs.empty()) {
         int curNode = bfs.front();
         bfs.pop();
+
+        if (visited[curNode]) break;
+        visited[curNode] = true;
         order.push_back(curNode);
         
         for (int adjNode : adjList[curNode]) {
