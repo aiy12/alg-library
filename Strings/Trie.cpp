ddc5ea7461a215c2de1d64a6d35fa5b9cebbc8fe
Anand Iyer <anandiyer1000@gmail.com>
2020-07-23 11:02:34 -0500

    Update Trie.cpp

--- Strings/Trie.cpp
+++ Strings/Trie.cpp
@@ -4,24 +4,20 @@ int cnt = 1;
 
 void insert(string s) {
     int pos = 0;
-    
     for (char c : s) {
         if (trie[pos][c - 'a'] == 0) 
             trie[pos][c - 'a'] = cnt++;
         pos = trie[pos][c - 'a'];
     }
-    
     endOfWord[pos] = true;
 }
 
 bool search(string s) {
     int pos = 0;
-    
     for (char c : s) {
         if (trie[pos][c - 'a'] == 0) 
             return false;
         pos = trie[pos][c - 'a'];
     }
-    
     return endOfWord[pos];
 }
