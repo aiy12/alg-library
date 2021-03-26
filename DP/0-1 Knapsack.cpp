480c6557b46b6de17cb7b87806cf266e5ba2db2b
Anand Iyer <anandiyer1000@gmail.com>
2021-03-26 14:23:13 -0500

    bug fix

--- DP/0-1 Knapsack.cpp
+++ DP/0-1 Knapsack.cpp
@@ -6,7 +6,7 @@ int Knapsack() {
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j <= w; j++) {
             if (i == 0 || j == 0) dp[i][j] = 0;
-            else if (weights[i] <= j) dp[i][j] = max(values[i-1] + dp[i-1][j-weights[i-1]], dp[i-1][j]);
+            else if (weights[i-1] <= j) dp[i][j] = max(values[i-1] + dp[i-1][j-weights[i-1]], dp[i-1][j]);
             else dp[i][j] = dp[i-1][j];
         }
     }
